816a18bd6c244229199f1bdc742a9108fdab298f
Jonathan Wall <jonathanwall@gmail.com>
2020-07-02 22:55:23 -0400

    add ping command

---
+++ cogs/ping.py
@@ -0,0 +1,24 @@
+import time
+
+from discord.ext import commands
+
+
+class ping(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+
+    @commands.command()
+    async def ping(self, ctx):
+        """ Returns the latency in milliseconds """
+        before = time.time()
+        message = await ctx.send("ms")
+        ping = (time.time() - before) * 1000
+        await message.edit(content=f"`{int(ping)}ms`")
+
+    @ping.error
+    async def ping_error(self, ctx, error):
+        await ctx.send(error)
+
+
+def setup(bot):
+    bot.add_cog(ping(bot))
