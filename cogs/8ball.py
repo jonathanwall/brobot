4bfa4c30641b721946ff5e676108f7f2799ac0d8
Jonathan Wall <jonathanwall@gmail.com>
2021-04-22 20:28:41 -0400

    add 8ball

---
+++ cogs/8ball.py
@@ -0,0 +1,42 @@
+import random
+
+from discord import Embed
+from discord.ext import commands
+
+
+class eight_ball(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+
+    @commands.command(name="8ball")
+    async def eight_ball(self, ctx):
+        """ shakes an 8ball """
+        embed = Embed(title="8ball")
+        choices = (
+            "As I see it, yes.",
+            "Ask again later.",
+            "Better not tell you now.",
+            "Cannot predict now.",
+            "Concentrate and ask again.",
+            "Don’t count on it.",
+            "It is certain.",
+            "It is decidedly so.",
+            "Most likely.",
+            "My reply is no.",
+            "My sources say no.",
+            "Outlook not so good.",
+            "Outlook good.",
+            "Reply hazy, try again.",
+            "Signs point to yes.",
+            "Very doubtful.",
+            "Without a doubt.",
+            "Yes.",
+            "Yes – definitely.",
+            "You may rely on it.",
+        )
+        embed.description = f"**{random.choice(choices)}**"
+        await ctx.send(embed=embed)
+
+
+def setup(bot):
+    bot.add_cog(eight_ball(bot))
