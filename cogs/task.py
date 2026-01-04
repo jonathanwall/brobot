3d33ed82c8cd3960a4223950a36a66f5209d7c3f
Jonathan Wall <jonathanwall@gmail.com>
2026-01-04 17:09:48 -0500

    add task.py

---
+++ cogs/task.py
@@ -0,0 +1,32 @@
+import logging
+from discord.ext import commands, tasks
+
+log = logging.getLogger(__name__)
+
+
+class Task(commands.Cog):
+    """A cog that demonstrates background tasks"""
+
+    def __init__(self, bot):
+        self.bot = bot
+        self.index = 0
+        self.printer.start()
+
+    def cog_unload(self):
+        self.printer.cancel()
+
+    @tasks.loop(seconds=5.0)
+    async def printer(self):
+        log.error(self.index)
+        self.index += 1
+
+    @printer.before_loop
+    async def before_printer(self):
+        await self.bot.wait_until_ready()
+
+
+def setup(bot):
+    try:
+        bot.add_cog(Task(bot))
+    except Exception as e:
+        log.error(f"Failed to load Task cog: {e}")
